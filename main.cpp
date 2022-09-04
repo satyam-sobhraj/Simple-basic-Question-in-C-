2f8f47e19ac9c27ea4ba0b82f0d86f038437474b
Satyam <satyamsobhraj819@gmail.com>
2022-09-05 01:20:23 +0530

    C++ Simple basic question

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // -----------------------------❤️❤️Print 1 to n❤️❤️----------------------------------------
+
+    // int n;
+    // cin>>n;
+
+    // int i=1;
+
+    // while(i<=n){
+    //     cout<<i;
+    //     i=i+1;
+    // }
+
+    // -------------------------------❤️❤️Sum of 1 to n❤️❤️---------------------------------------
+
+    // int n;
+    // cin >> n;
+
+    // int i = 1;
+    // int sum = 0;
+
+    // while (i <= n)
+    // {
+    //     sum = sum + i;
+    //     i = i + 1;
+    // }
+    // cout << "value of sum: " << sum << endl;
+
+    // -----------------------❤️❤️Sum of all even no. from 1 to n❤️❤️---------------------------------
+
+    // int n;
+    // cin >> n;
+
+    // int num = 2;
+    // while (num < n)
+    // {
+    //     cout << num << endl;
+    //     num = num + 2;
+    // }
+
+    // --------------------------❤️❤️Print odd number 1 to n❤️❤️---------------------------------
+
+    int n;
+    cin >> n;
+
+    int num = 1;
+    while (num <= n)
+    {
+        cout << num << endl;
+        num = num + 2;
+    }
+}
